7561a6b4071e13114080ffa97e03d9af261842c4
smthwentroong <kirill.kopienko@gmail.com>
2024-03-04 00:22:53 +0300

    added paging logic

--- src/main/java/kirill/kopienko/springbootcrud/controller/TaskController.java
+++ src/main/java/kirill/kopienko/springbootcrud/controller/TaskController.java
@@ -9,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static java.util.Objects.isNull;
 
@@ -24,10 +26,15 @@ public class TaskController {
 
     @GetMapping("/")
     public String tasks(Model model,
-                        @RequestParam(name = "limit", required = false, defaultValue = "10") int limit,
-                        @RequestParam(name = "offset", required = false, defaultValue = "0") int offset) {
-        List<Task> listToShow = taskService.findAll(limit, offset);
+                        @RequestParam(name = "page", required = false, defaultValue = "1") int page,
+                        @RequestParam(name = "limit", required = false, defaultValue = "10") int limit) {
+        List<Task> listToShow = taskService.findAll(limit, (page - 1) * limit);
         model.addAttribute("tasksList", listToShow);
+        int totalPages = (int) Math.ceil(1.0 * taskService.getAllCount() / limit);
+        if (totalPages > 1) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages).boxed().collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
         return "tasks";
     }
 
