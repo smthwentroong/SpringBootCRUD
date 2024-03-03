2432c16b89e55735597be0583f78db7916952524
smthwentroong <kirill.kopienko@gmail.com>
2024-03-03 16:03:14 +0300

    init commit

---
+++ src/test/java/kirill/kopienko/springbootcrud/SpringBootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package kirill.kopienko.springbootcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
