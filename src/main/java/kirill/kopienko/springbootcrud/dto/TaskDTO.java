d2e5ab500e291abec3e907c2901ba0d09ff16c21
smthwentroong <kirill.kopienko@gmail.com>
2024-03-03 16:48:11 +0300

    added dto layer

---
+++ src/main/java/kirill/kopienko/springbootcrud/dto/TaskDTO.java
@@ -0,0 +1,25 @@
+package kirill.kopienko.springbootcrud.dto;
+
+import kirill.kopienko.springbootcrud.domain.Status;
+
+public class TaskDTO {
+    private String description;
+
+    private Status status;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
